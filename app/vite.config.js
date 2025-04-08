98cd42d3c293672f89bad4749037291c3bd98bad
Braily Guzman <brailyg@outlook.com>
2025-04-07 22:05:38 -0400

    Add vite config and lint options

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+const SERVER_PORT = 8080;
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: `http://localhost:${SERVER_PORT}`,
+        changeOrigin: true,
+      },
+    },
+  },
+});

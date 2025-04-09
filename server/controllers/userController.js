10723d60e5203d7e7e461fca9cedfcb65743c0c9
Braily Guzman <brailyg@outlook.com>
2025-04-09 12:06:29 -0400

    Add User controllers

---
+++ server/controllers/userController.js
@@ -0,0 +1,11 @@
+const getUserController = (req, res) => {};
+const newUserController = (req, res) => {};
+const editUserController = (req, res) => {};
+const deleteUserController = (req, res) => {};
+
+module.exports = {
+  getUserController,
+  newUserController,
+  editUserController,
+  deleteUserController,
+};

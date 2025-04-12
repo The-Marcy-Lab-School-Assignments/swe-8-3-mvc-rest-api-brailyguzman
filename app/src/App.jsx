6fc1d21b74c046f0bc4b1c7851cd3ca2b5c38945
Braily Guzman <brailyg@outlook.com>
2025-04-12 10:45:37 -0400

    Add users route to App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home.jsx';
+import UserDetails from './pages/UserDetails.jsx';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/users/:id" element={<UserDetails />} />
     </Routes>
   );
 };

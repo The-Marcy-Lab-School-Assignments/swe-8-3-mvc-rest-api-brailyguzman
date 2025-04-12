4b5bf795395f95fd69ccc0b6eacd22a70b566a23
Braily Guzman <brailyg@outlook.com>
2025-04-12 10:46:00 -0400

    Add endpoints to the server

--- server/index.js
+++ server/index.js
@@ -6,25 +6,33 @@ const {
   newUserController,
   editUserController,
   deleteUserController,
+  getUsersController,
 } = require('./controllers/userController.js');
 
 const app = express();
-const port = 8080;
+/*
+When I send a PATCH request to port 8080 on my computer,
+the request hangs and never sends back a response.
+
+Using a different port like 3000 fixes it, not sure
+what could be causing it.
+*/
+const port = 3000;
 
 const pathToFrontendDist = path.join(__dirname, '../app/dist');
 
 const serveStatic = express.static(pathToFrontendDist);
 const parseJSON = express.json();
 
-app
-  .route('/api/user')
-  .get(getUserController)
-  .post(newUserController)
-  .patch(editUserController)
-  .delete(deleteUserController);
-
 app.use(logRoutes);
 app.use(serveStatic);
 app.use(parseJSON);
+
+app.post('/api/users', newUserController);
+app.get('/api/users', getUsersController);
+
+app.get('/api/users/:id', getUserController);
+app.patch('/api/users/:id', editUserController);
+app.delete('/api/users/:id', deleteUserController);
 
 app.listen(port, () => console.log(`listening at http://localhost:${port}`));

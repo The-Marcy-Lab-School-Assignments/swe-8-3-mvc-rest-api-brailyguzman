be71a76958d065be27bf270886f1015ebee03d87
Braily Guzman <brailyg@outlook.com>
2025-04-12 10:46:15 -0400

    Update port to fix request issue

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-const SERVER_PORT = 8080;
+
+/*
+When I send a PATCH request to port 8080 on my computer,
+the request hangs and never sends back a response.
+
+Using a different port like 3000 fixes it, not sure
+what could be causing it.
+*/
+const SERVER_PORT = 3000;
 
 // https://vitejs.dev/config/
 export default defineConfig({
